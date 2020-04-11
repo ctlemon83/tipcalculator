59610f36e2645d69cb81f8aa8afa9dd478c9437e
Colin Lemon <ctlemon83@gmail.com>
2020-04-11 00:21:32 -0500

    Adding DOM manipulation
    Added complete function that  modifies the DOM

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-function tipCalculator(subTotal) {
-	var tipPercent = prompt('Tip Percentage')/100;
-	var finalTotal = subTotal * (1 + tipPercent);
-	alert('Your final total is $' + finalTotal + '.');
+function tipCalculator() {
+	var subTotal = document.getElementById('sub').value;
+	var tipPercent = document.getElementById('percentage').value;
+	var tip = (tipPercent/100) + 1;
+	var finalTotal = (tip * subTotal).toFixed(2);
+	document.getElementById('final').style.visibility='visible';
+	document.getElementById('total').innerHTML = '$'+finalTotal;
+	var subTotal = document.getElementById('sub').value = ' ';
+	var tipPercent = document.getElementById('percentage').value = ' ';
 }
-
-tipCalculator(88);
